ad5c6e4714a8be517cb86be99ad0eebc96bac46a
Matteo 'Peach' Pescarin <peach@smartart.it>
2012-09-11 10:21:54 +0100

    fixed db configuration file sourcing

--- backup/redmine_backup.sh
+++ backup/redmine_backup.sh
@@ -19,15 +19,18 @@ GIT_SERVER=`git config remote.origin.url`
 function usage() {
 echo -e "Usage: `basename $2` [ -v | -r | -h ] [commit msg]
 
-When called without parameters, the Redmine database and files are dumped to
-git-repo in ${REDMINE_HOME}, then the git-repo is pushed to ${GIT_SERVER}.
+When called without parameters, the Redmine database and files are 
+dumped to git-repo in ${REDMINE_HOME}, then the git-repo is pushed
+to ${GIT_SERVER}.
 
-When the first parameter is none of the ones below, the same backup procedure
-is done, but the commit message is the parameter list instead of the date.
+When the first parameter is none of the ones below, the same backup
+procedure is done, but the commit message is the parameter list 
+instead of the date.
 
 \t-v: be verbose
-\t-r: Beforehand, check out the desired version of the Redmine database from 
-\t\tgit-repo. This command will restore that version into Redmine.
+\t-r: Beforehand, check out the desired version of the Redmine database
+\t\tfrom git-repo.
+\t\tThis command will restore that version into Redmine.
 \t-h: Print this help text.
 \n"
 }
@@ -71,9 +74,11 @@ then
     echo "${DB_CONFIG} not found, check carefully."
     exit $E_GENERROR
 fi
-DATABASE=`cat ${DB_CONFIG} | sed -rn 's/ *database: (.+)/\1/p'`
-USERNAME=`cat ${DB_CONFIG} | sed -rn 's/ *username: (.+)/\1/p'`
-PASSWORD=`cat ${DB_CONFIG} | sed -rn 's/ *password: (.+)/\1/p'`
+# here we are just guessing production will be 
+# at the very beginning of the file
+DATABASE=`cat ${DB_CONFIG} | sed -rn 's/ *database: (.+)/\1/p' | head -n 1`
+USERNAME=`cat ${DB_CONFIG} | sed -rn 's/ *username: (.+)/\1/p' | head -n 1`
+PASSWORD=`cat ${DB_CONFIG} | sed -rn 's/ *password: (.+)/\1/p' | head -n 1`
 
 if [ -n $BE_VERBOSE ] 
 then
@@ -87,24 +92,26 @@ cd "${REDMINE_HOME}"
 # Restore
 if [ $DO_RESTORE ]
 then
-  /usr/bin/mysql --user=${USERNAME} --password=${PASSWORD} $DATABASE < redmine.sql
-  cp -f [!r][!e][!d][!m][!i][!n][!e]* $FILES
+    if [ -n $BE_VERBOSE ]; then echo "Restoring from $DATABASE";
+    /usr/bin/mysql --user=${USERNAME} --password=${PASSWORD} $DATABASE < redmine.sql
+    cp -f [!r][!e][!d][!m][!i][!n][!e]* $FILES
 
 # Backup
 else
-  if [ "$1" ]; then COMMITT_MSG="$@";
-  /usr/bin/mysqldump --user=${USERNAME} --password=${PASSWORD} --skip-extended-insert $DATABASE > redmine.sql
-  cp -f ${FILES}/* .
-  git add *
-  git commit -m "$MSG" 
-  git push --all origin
+    if [ -n $BE_VERBOSE ]; then echo "Backing up $DATABASE";
+    if [ "$1" ]; then COMMITT_MSG="$@";
+    /usr/bin/mysqldump --user=${USERNAME} --password=${PASSWORD} --skip-extended-insert $DATABASE > redmine.sql
+    cp -f ${FILES}/* .
+    git add *
+    git commit -m "$MSG" 
+    git push --all origin
 fi
 
 # something has gone bad, we have to report it
 if [ $? -ne 0 ]
 then
-   echo "Whoops! Something went wrong!"
-   exit $E_GENERROR
+    echo "Whoops! Something went wrong!"
+    exit $E_GENERROR
 fi
 
 exit
