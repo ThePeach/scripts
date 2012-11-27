a81f67b46df940e5dedec97b1ab272cb034b60a3
Matteo 'Peach' Pescarin <peach@smartart.it>
2012-11-27 13:21:26 +0000

    progress with script functionality

--- httpdocs-synch.sh
+++ httpdocs-synch.sh
@@ -25,8 +25,9 @@ function usage() {
     echo -e "Syntax: `basename $0` [-h|-v] [-s <SYNC_DIR_1>[,<SYNC_DIR_2>[,...]]] [-b BACKUP_DIR] <SOURCE_DIR> <TARGET_DIR>
 \t-h: shows this help
 \t-v: be verbose
+\t-n: dry run (don't actually execute the commands)
 \t-s <SYNC_DIR_1>[,<SYNC_DIR_2>[,...]]: (comma-separated) list of dirs paths to be
-\t\t synched back. The paths must be relative to the exec dir.
+\t\t synched back. The paths can be absolute or relative to the target dir.
 \t-b <BACKUP_DIR>: the backup directory where the to put the backup (tar.bz2 format)
 \t<SOURCE_DIR>: directory to be used as source
 \t<TARGET_DIR>: the destination directory, if not existing it'll be created.
@@ -43,21 +44,27 @@ function error() {
     usage
 }
 
+function quit() {
+    IFS=$OLD_IFS
+    exit $1
+}
+
 if [ $# -eq "$NO_ARGS" ]
 then
     version
     usage
-    exit $E_OPTERROR
+    quit $E_OPTERROR
 fi
 
 # The expected flags are
 #  h v r
-while getopts ":hvs:b:" Option
+while getopts ":hnvs:b:" Option
 do
     case $Option in
         h ) version
             usage
-            exit 0;;
+            quit 0;;
+        n ) DRYRUN_OPT=" -n ";;
         v ) BE_VERBOSE=true;;
         s ) SYNC_DIRS=$OPTARG
 			SYNC_BACK=true;;
@@ -72,28 +79,65 @@ done
 shift $(($OPTIND - 1))
 
 # check the dest and source dirs are ok and normalise the paths
+# the source must have a / delimiter
+# the target must not
 if [ $# -eq 2 ]
 then
     SOURCE_DIR=$1
+    [ `echo $SOURCE_DIR | grep [^/]$` ] && SOURCE_DIR="$SOURCE_DIR/"
     TARGET_DIR=$2
-else if [ $# -eq 1 ]
+    [ `echo $TARGET_DIR | grep /$` ] && TARGET_DIR="${TARGET_DIR%?}"
+elif [ $# -eq 1 ]
 then
     TARGET_DIR=$1
-else
-    
+fi
+# ensure source dir exists
+if [ ! -e $SOURCE_DIR ]
+then
+    echo "Source dir '$SOURCE_DIR' not found"
+    quit $E_GENERROR 
+fi
+# ensure target dir exists
+if [ ! -e $TARGET_DIR ]
+then
+    echo "Target dir '$TARGET_DIR' not found"
+    quit $E_GENERROR 
+fi
 
+[[ -n $BE_VERBOSE ]] && echo "SOURCE_DIR: $SOURCE_DIR"
+[[ -n $BE_VERBOSE ]] && echo "TARGET_DIR: $TARGET_DIR"
+    
 # Split the directories to sync back
 if [ -n $SYNC_BACK ]
 then
     read -ar SYNC_DIRS <<< "$SYNC_DIRS"
     for dir in $SYNC_DIRS
     do
-        if [[ -e $dir ]]
-        rsync -az --delete $dir
+        if [[ ! -e "${TARGET_DIR}/${dir}" ]]
+        then
+            echo "Sync-back dir $TARGET_DIR/$dir not found"
+            quit $E_GENERROR
+        elif [ -n $BE_VERBOSE ]
+        then
+            echo "Sync-back dir found: $TARGET_DIR/$dir"
+        fi
+    done
+    # if we are here we can start doing the sync-back
+    for dir in $SYNC_DIRS
+    do
+        [ `echo $dir | grep /$` ] && dir="${dir%?}"
+        [[ -n $BE_VERBOSE ]] && echo rsync -az --delete $DRYRUN_OPT "$TARGET_DIR/$dir" "${SOURCE_DIR}${dir}/"
+        # rsync \
+        #     -az --progress \
+        #     --delete \
+        #     $DRYRUN_OPT \
+        #     "$TARGET_DIR/$dir" "${SOURCE_DIR}${dir}/"
     done
 fi
 
-rsync -az --delete $SOURCE_DIR $SYNCBACK
+# rsync \
+#     -az --delete \
+#     $DRYRUN_OPT \
+#     "$SOURCE_DIR" "$TARGET_DIR"
 
-IFS=$OLD_IFS
-exit
+quit 0
